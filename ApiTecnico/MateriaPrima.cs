0fc10a439fe271e27e4be65bc14451889c3c9fcf
Jose_Carneiro <alunotaoaprendiz@gmail.com>
2024-07-01 09:28:30 -0300

    01072024 - Subindo o meu primeiro projeto

---
+++ ApiTecnico/MateriaPrima.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    public class MateriaPrima
+    {
+       
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Quantidade { get; set; }
+        public string UnidadeMedida { get; set; }
+        public decimal PrecoPorUnidade { get; set; }
+
+        
+        public MateriaPrima(int id, string nome, double quantidade, string unidadeMedida, decimal precoPorUnidade)
+        {
+            Id = id;
+            Nome = nome;
+            Quantidade = quantidade;
+            UnidadeMedida = unidadeMedida;
+            PrecoPorUnidade = precoPorUnidade;
+        }
+
+        
+        public decimal CalcularCustoTotal()
+        {
+            return PrecoPorUnidade * (decimal)Quantidade;
+        }
+
+       
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Quantidade: {Quantidade} {UnidadeMedida}");
+            Console.WriteLine($"Preço por Unidade: {PrecoPorUnidade:C}");
+            Console.WriteLine($"Custo Total: {CalcularCustoTotal():C}");
+        }
+    }
+}
